ab077db8822088d3dc96232ea58ef88c7224916b
nirmal100-ux <nirmalstha783@gmail.com>
2023-08-28 09:14:32 +0545

    change here

--- src/constant/Constant.js
+++ src/constant/Constant.js
@@ -1,3 +1,3 @@
-export const baseURL = "http://192.168.1.67:5000";
+//export const baseURL = "http://192.168.1.67:5000";
 
-//export const baseURL = "https://job-portal-backend-wki7.onrender.com";
+export const baseURL = "https://job-back-chhy.onrender.com";
